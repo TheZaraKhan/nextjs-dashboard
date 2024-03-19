3d96b588b97fbf272b0510878a2520244ccc3bb4
Zara Khan <ZaraMustafa@ymail.com>
2024-03-20 03:33:01 +0500

    deploy

--- app/lib/placeholder-data.js
+++ app/lib/placeholder-data.js
@@ -2,9 +2,9 @@
 // https://nextjs.org/learn/dashboard-app/fetching-data
 const users = [
   {
-    id: '410544b2-4001-4271-9855-fec4b6a6442a',
-    name: 'User',
-    email: 'user@nextmail.com',
+    id: '410544b2-4001-4271-9855-fec4b6a6442b',
+    name: 'john doe',
+    email: 'john@nextmail.com',
     password: '123456',
   },
 ];
---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Nunito, Lusitana } from 'next/font/google';
+
+export const nunito = Nunito({ subsets: ['latin'] });
+export const lusitana = Lusitana({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+});
